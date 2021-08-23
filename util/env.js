ae79e6b1899afb11d5fa1027a8939a81bfbbc7a0
raneyj <raney.jack99@gmail.com>
2021-08-23 15:11:25 -0500

    Copied bookstore

---
+++ util/env.js
@@ -0,0 +1,6 @@
+const APP_ROOT = document.getElementById('app-root');
+
+export default {
+    rootDiv: APP_ROOT,
+    apiUrl: 'http://bookstoreapi-env.eba-ysp2j7xq.us-east-1.elasticbeanstalk.com'
+}
