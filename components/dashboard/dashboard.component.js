c28ef0ea9fd2bf73a120fddeba8cb67f590a9118
ColbyDub <cduball@outlook.com>
2021-08-25 21:42:03 -0500

    dashboard update

--- components/dashboard/dashboard.component.js
+++ components/dashboard/dashboard.component.js
@@ -1,11 +1,14 @@
 import { ViewComponent } from '../view.component.js';
 import state from '../../util/state.js';
 import router from '../../app.js';
+import env from '../../util/env.js';
 
 DashboardComponent.prototype = new ViewComponent('dashboard');
 function DashboardComponent() {
 
     let welcomeUserElement;
+    let tableBodyElement;
+    let errorMessageElement;
 
     this.render = function() {
 
@@ -16,23 +19,100 @@ function DashboardComponent() {
             return;
         }
 
-        let currentUsername = state.authUser.username;
+        let authUser = state.authUser;
 
         DashboardComponent.prototype.injectStylesheet();
         DashboardComponent.prototype.injectTemplate(() => {
 
-            if(state.authUser.faculty){
-                welcomeUserElement = document.getElementById('Dashboard-title');
+            welcomeUserElement = document.getElementById('Dashboard-title');
+            tableBodyElement = document.getElementById('class-table-body');
+            errorMessageElement = document.getElementById('error-msg');
+
+
+            AppendUsersClasses(authUser.id);
+
+
+
+            if(authUser.faculty){
+
                 welcomeUserElement.innerText = "Faculty Dashboard";
+
+
+
             } else {
-                welcomeUserElement = document.getElementById('Dashboard-title');
+
                 welcomeUserElement.innerText = "Student Dashboard";
+
+
             }
             
             window.history.pushState('dashboard', 'Dashboard', '/dashboard');
 
         });
 
+    }
+
+    async function AppendUsersClasses(id){
+
+        console.log('appending the classes')
+        
+        let response = await fetch(`${env.apiUrl}/classes/?id=${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': state.JWT
+            },
+        })
+
+        let data = await response.json();
+
+        if(response.status!=200)
+            updateErrorMessage(data.message);
+        else{
+            
+            for(c in data){
+                let row = document.createElement('tr');
+                let idCell = document.createElement('td');
+                let titleCell = document.createElement('td');
+                let professorCell = document.createElement('td');
+                let descriptionCell = document.createElement('td');
+                let capacityCell = document.createElement('td');
+    
+    
+                // Append cells to the row
+                row.appendChild(idCell);
+                row.appendChild(nameCell);
+                row.appendChild(titleCell);
+                row.appendChild(professorCell);
+                row.appendChild(descriptionCell);
+                row.appendChild(capacityCell);
+
+                document.getElementById('class-table-body').appendChild(row);
+
+                
+                idCell.innerText = c.id;
+                titleCell.innerText = c.name;
+                descriptionCell.innerText = c.description;
+                capacityCell.innerText = c.capacity;
+
+                let professors = c.faculty;
+                for(p in professors)
+                    professorCell.innerText += (p + "\n");
+            }
+
+        }
+
+    }
+
+    
+    function updateErrorMessage(errorMessage) {
+        if (errorMessage) {
+            errorMessageElement.removeAttribute('hidden');
+            errorMessageElement.innerText = errorMessage;
+        } else {
+            errorMessageElement.setAttribute('hidden', 'true');
+            errorMessageElement.innerText = '';
+        }
     }
 
 }
