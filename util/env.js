46055ff203371ca1ec00a91aac3846d918438a65
raneyj <raney.jack99@gmail.com>
2021-08-30 08:31:57 -0500

    changed api endpoint

--- util/env.js
+++ util/env.js
@@ -2,6 +2,6 @@ const APP_ROOT = document.getElementById('app-root');
 
 export default {
     rootDiv: APP_ROOT,
-    //apiUrl: 'http://Registrarapp-env.eba-pxifbtnp.us-east-2.elasticbeanstalk.com'
-    apiUrl: 'http://localhost:8080/registrar'
+    apiUrl: 'http://Registrarapp-env.eba-pxifbtnp.us-east-2.elasticbeanstalk.com'
+    //apiUrl: 'http://localhost:8080/registrar'
 }
